bef2f5a4674e610cf85f0c5b894c426905103bdd
phakphoum38-stack <phakphoum38@gmail.com>
2026-03-21 10:32:30 +0700

    Create server.py

---
+++ web/server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+
+    return "Hackintosh Auto Builder Web UI"
+
+if __name__ == "__main__":
+
+    app.run(
+        host="0.0.0.0",
+        port=5000
+    )
